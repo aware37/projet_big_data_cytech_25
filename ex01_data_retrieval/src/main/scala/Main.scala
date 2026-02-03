a61d4989491fca0bd7bfed16a4b1af4da8e466b4
aware37 <awareaware37@gmail.com>
2026-02-03 02:42:05 +0100

    modif docker + exercice1

---
+++ ex01_data_retrieval/src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+import io.minio.{MinioClient, UploadObjectArgs, PutObjectArgs}
+import java.nio.file.{Files, Paths}
+import java.net.URL
+import scala.util.Using
+
+
+
+object Main extends App {
+
+    val endpoint = "http://localhost:9000"
+    val accessKey = "minio"
+    val secretKey = "minio123"
+    val bucketName = "nyc-raw"
+
+    val fileURL = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-01.parquet"
+    val localURL = "../data/raw/yellow_tripdata_2025-01.parquet"
+
+    // Configuration du client MinIO
+    val minioClient = MinioClient.builder()
+        .endpoint(endpoint)
+        .credentials(accessKey, secretKey)
+        .build()
+
+    val reponse = requests.get(fileURL)
+    Files.write(Paths.get(localURL), reponse.bytes)
+
+    minioClient.uploadObject(
+        UploadObjectArgs.builder()
+            .bucket(bucketName)
+            .`object`("yellow_tripdata_2025-01.parquet")
+            .filename(localURL)
+            .contentType("application/octet-stream")
+            .build()
+    )
+
+    Using.resource(new URL(fileURL).openStream()) { inputStream =>
+      val connection = new URL(fileURL).openConnection()
+      val size = connection.getContentLengthLong
+      
+      minioClient.putObject(
+        PutObjectArgs.builder()
+          .bucket(bucketName)
+          .`object`("Stream_yellow_tripdata_2025-01.parquet")
+          .stream(inputStream, size, -1)
+          .contentType("application/octet-stream")
+          .build()
+      )
+
+    }
+}
